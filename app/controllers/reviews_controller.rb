c36d78ee7b581de1ce1110a51229597ba3eb5bad
Ethan Vandal <ethanvandal18@gmail.com>
2025-07-25 07:07:44 -0500

    added csv attribution

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,10 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.includes(:product, :satisfaction, :importance)
+                     .order(created_at: :desc)
+                     .page(params[:page])
+  end
+
   def show
     @review = Review.find(params[:id])
     @product = @review.product
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,23 @@
 module ReviewsHelper
+  def satisfaction_color(level)
+    case level.downcase
+    when 'very satisfied' then 'success'
+    when 'satisfied' then 'primary'
+    when 'neutral' then 'warning'
+    when 'unsatisfied' then 'danger'
+    when 'very unsatisfied' then 'dark'
+    else 'secondary'
+    end
+  end
+
+  def importance_color(level)
+    case level.downcase
+    when 'very important' then 'danger'
+    when 'important' then 'warning'
+    when 'somewhat important' then 'info'
+    when 'neutral' then 'secondary'
+    when 'not at all important' then 'light text-dark'
+    else 'secondary'
+    end
+  end
 end
