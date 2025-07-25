4b3d37a20f98a0d2dac8edbddd98b43e750b1026
Ethan Vandal <ethanvandal18@gmail.com>
2025-07-25 07:36:58 -0500

    tweaked erd page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get "erd/show"
   root 'products#index'
   get 'about', to: 'pages#about'
+  get 'erd', to: 'erd#show', as: 'erd'
+
 
   resources :products, only: [:index, :show] do
     resources :reviews, only: [:index]
